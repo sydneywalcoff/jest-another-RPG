ac95f66d33e601700facadc545544d464dd78169
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-03-30 12:44:59 -0700

    passing tests for the inventory and getstats methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,5 @@
 
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
@@ -16,4 +16,25 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test('gets player\'s stats as an object', () => {
+    const player = new Player ('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+    console.log(player.inventory)
+
+    expect(player.inventory).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
